e624d7dcf734629c3bc0e2d248bf0604d970a773
Jan Schulte <jan@janschulte.com>
2017-12-23 12:55:10 +0100

    Updated track finding

--- R/optimum-track-finding.R
+++ R/optimum-track-finding.R
@@ -39,5 +39,15 @@ dms = lapply(zps, function(i) {
   return(rst)
 })
 
-dem = merge(dms, filename = "data/ASTGTM2_NxxE0xx.tif", datatype = "INT2U")
+dem = merge(dms)
+dem = crop(dem, ext, snap = "out", filename = "data/ASTGTM2_NxxE0xx_dem.tif", 
+           datatype = "INT2U", overwrite = TRUE)
+
+spplot(dem, scales = list(draw = TRUE, y = list(rot = 90)), 
+       sp.layout = list("sp.points", as(breweries, "Spatial"), pch = 20, col = "black"), 
+       col.regions = terrain.colors(111), at = seq(0, 1100, 10))
+
+# Calculate slopes
+slp = terrain(dem,unit = "degrees", filename = "data/ASTGTM2_NxxE0xx_slp.tif")
+asp = terrain(dem,opt='aspect',unit = "degrees", filename = "data/ASTGTM2_NxxE0xx_asp.tif")
 
