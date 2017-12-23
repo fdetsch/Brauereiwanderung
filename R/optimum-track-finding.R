76177b38c7d5611d4c96ec155edca52996813b76
fdetsch <flowla88@gmail.com>
2017-12-23 19:09:31 +0100

    Update: OSM processing.

--- R/optimum-track-finding.R
+++ R/optimum-track-finding.R
@@ -43,11 +43,74 @@ dem = merge(dms)
 dem = crop(dem, ext, snap = "out", filename = "data/ASTGTM2_NxxE0xx_dem.tif", 
            datatype = "INT2U", overwrite = TRUE)
 
-spplot(dem, scales = list(draw = TRUE, y = list(rot = 90)), 
-       sp.layout = list("sp.points", as(breweries, "Spatial"), pch = 20, col = "black"), 
-       col.regions = terrain.colors(111), at = seq(0, 1100, 10))
+# ## visualize data (optional)
+# spplot(dem, scales = list(draw = TRUE, y = list(rot = 90)), 
+#        sp.layout = list("sp.points", as(breweries, "Spatial"), pch = 20, col = "black"), 
+#        col.regions = terrain.colors(111), at = seq(0, 1100, 10))
 
-# Calculate slopes
-slp = terrain(dem,unit = "degrees", filename = "data/ASTGTM2_NxxE0xx_slp.tif")
-asp = terrain(dem,opt='aspect',unit = "degrees", filename = "data/ASTGTM2_NxxE0xx_asp.tif")
+## calculate slope and aspect
+slp = terrain(dem, unit = "degrees", filename = "data/ASTGTM2_NxxE0xx_slp.tif")
+asp = terrain(dem, opt = 'aspect',unit = "degrees", 
+              filename = "data/ASTGTM2_NxxE0xx_asp.tif")
 
+
+### openstreetmap data -----
+
+srv = "http://download.geofabrik.de/europe/germany/bayern/"
+zps = sapply(c("ober", "mittel", "unter"), function(region) {
+  onl = paste0(srv, region, "franken-latest-free.shp.zip")
+  ofl = gsub(srv, "inst/extdata/", onl)
+  
+  ifMissing(ofl, invisible, download.file, "destfile", url = onl)
+})
+
+library(parallel)
+cl = makePSOCKcluster(3L)
+
+shp = parLapply(cl, zps, function(i) {
+  ifl = unzip(i, list = TRUE)$Name
+  ids = grep("roads", ifl)
+  ofl = unzip(i, files = ifl[ids], exdir = gsub(".shp.zip$", "", i)
+              , overwrite = FALSE)
+  
+  raster::shapefile(ofl[grep(".shp$", ofl)])
+})
+
+rds = do.call("rbind", shp)
+
+cds = data.frame(code = unique(rds@data$code), fclass = unique(rds@data$fclass))
+cds = cds[order(cds$code), ]
+row.names(cds) = as.character(1:nrow(cds))
+saveRDS(cds, file = "inst/extdata/fclass-codes.rds")
+
+rds@data = rds@data[, c("osm_id", "code", "name", "ref", "maxspeed")]
+saveRDS(rds, file = "inst/extdata/franken-roads.rds")
+
+## http://download.geofabrik.de/europe/germany/bayern.html
+osm = dir("inst/extdata", pattern = "franken$", full.names = TRUE)
+ids = sapply(c("ober", "mittel", "unter"), function(i) grep(i, osm))
+osm = osm[ids]
+
+
+### spatial analysis -----
+
+brw = as(breweries, "Spatial")
+brw = spTransform(brw, CRS("+init=epsg:32632"))
+
+## breweries with a neighboring brewery in a 2-km radius
+ids = sapply(seq_along(brw), function(i) {
+  bff = rgeos::gBuffer(brw[i, ], width = 2e3, quadsegs = 25)
+  xtr = over(brw, bff)
+  length(which(!is.na(xtr))) > 1
+})
+
+brw = brw[ids, ]
+
+## breweries with at least 4 neighboring breweries in a 10-km radius
+ids = sapply(seq_along(brw), function(i) {
+  bff = rgeos::gBuffer(brw[i, ], width = 1e4, quadsegs = 50)
+  xtr = over(brw, bff)
+  length(which(!is.na(xtr))) >= 4
+})
+
+brw = brw[ids, ]
