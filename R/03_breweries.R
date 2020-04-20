39c9f3ec1060717ad284eb76f35545798075a74b
fdetsch <fdetsch@web.de>
2020-04-20 17:13:50 +0200

    breweries retrieval from osm.

--- R/03_breweries.R
+++ R/03_breweries.R
@@ -1,25 +1,72 @@
 ### . osmdata (https://cran.r-project.org/web/packages/osmdata/vignettes/osmdata.html) ----
 
+### ENVIRONMENT ====
+
 ## load packages
 library(osmdata)
-library(sf)
 library(mapview)
 
+## define functions
+rbindsflist = function(x) {
+  dat = data.table::rbindlist(
+    x
+    , fill = TRUE
+  )
+  
+  sf::st_set_geometry(dat, "geometry")
+}
+
+
+### DATA RETRIEVAL ====
+
 ## find brewing specific key-value pairs
-q = opq(bbox = c(9.462785, 48.900742, 11.935392, 50.441619)) %>% 
-  add_osm_feature("craft", "brewery") %>%
-  add_osm_feature("microbrewery", "yes") %>% 
-  osmdata_sf()
+q = mapply(
+  function(key, value) {
+    opq(
+      bbox = c(9.462785, 48.900742, 11.935392, 50.441619)
+      , timeout = 90
+    ) %>% 
+      add_osm_feature(
+        key = key
+        , value = value
+      ) %>%
+      osmdata_sf()
+  }
+  , key = c("craft", "microbrewery")
+  , value = c("brewery", "yes")
+  , SIMPLIFY = FALSE
+)
+
+## bind point geometries
+pts = rbindsflist(
+  lapply(
+    seq(q)
+    , function(i) {
+      q[[i]]$osm_points
+    }
+  )
+)
+
+## bind polygon geometries
+pys = rbindsflist(
+  lapply(
+    seq(q)
+    , function(i) {
+      q[[i]]$osm_polygons
+    }
+  )
+)
 
 ## eliminate joint point/polygon venues in favor of polygons
-dpl = st_intersects(q$osm_points, q$osm_polygons)
-dpl = sapply(dpl, function(x) length(x) > 0)
+dpl = suppressMessages(
+  sf::st_intersects(
+    pts
+    , pys
+  )
+) %>% lengths > 0
 
-mapview(q$osm_points[, dpl]) + q$osm_polygons
-
-pts1 = q$osm_points[!dpl, ]
-pts2 = st_centroid(pts2)
+# mapview(pts[dpl, ]) + pys # duplicate points and polygons
 
 ## display data
-m = mapview(pts1) + pts2
+m = mapview(pts[!dpl, ]) + pys + suppressWarnings(sf::st_centroid(pys))
 m
